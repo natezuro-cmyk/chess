1438c9d75d0f425595cde1faca81207bb1708fb1
natezuro-cmyk <natezuro@gmail.com>
2026-03-11 17:02:49 -0600

    get user done

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -66,6 +66,19 @@ public class MySqlDataAccess implements DataAccess {
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()){
+            String statement = "SELECT username, password, email FROM users WHERE username = ?";
+            try(var preparedStatement = conn.prepareStatement(statement)){
+                preparedStatement.setString(1, username);
+                ResultSet data = preparedStatement.executeQuery();
+                if(data.next()){
+                    return new UserData(data.getString("username"),
+                            data.getString("password"),data.getString("email"));
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to get user from database", ex);
+        }
         return null;
     }
 
