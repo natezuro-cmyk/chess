0848bff00de336c758885d3b8ffeae7b72493ce9
natezuro-cmyk <natezuro@gmail.com>
2026-03-26 15:48:44 -0600

    facade done, setting up repl

--- client/src/main/java/client/PostLogin.java
+++ client/src/main/java/client/PostLogin.java
@@ -1,20 +1,91 @@
 package client;
 
+import model.AuthData;
+import model.GameData;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class PostLogin {
-    public PostLogin(ServerFacade facade){}
+    List<GameData> games;
+    private String authToken = null;
+    private ServerFacade facade;
 
-    public String eval(String input, String authToken){}
+    public PostLogin(ServerFacade facade){
+        this.facade = facade;
+    }
 
-    private String help(){}
+    public String eval(String input, String authToken){
+        try {
+            String[] tokens = input.toLowerCase().split(" ");
+            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "help" -> help();
+                case "logout" -> logout(authToken);
+                case "list" -> listGames(authToken);
+                case "create" -> createGame(params, authToken);
+                case "play" -> playGame(params, authToken);
+                case "observe" -> observeGame(params);
+                case "quit" -> "quit";
+                default -> "Unknown command. Type 'help' to see available commands.";
+            };
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
 
-    private String logout(String authToken){}
+    private String help(){
+        return """
+                - help
+                - logout
+                - list
+                - create
+                - play
+                - observe
+                - quit
+                """;
+    }
 
-    private String listGames(String authToken){}
+    private String logout(String authToken) throws Exception{
+        facade.logout(authToken);
+        this.authToken = null;
+        return String.format("You are not logged in");
+    }
 
-    private String createGame(String[] params, String authToken){}
+    private String listGames(String authToken) throws Exception {
+        games = facade.listGames(authToken);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= games.size(); i++){
+            GameData game = games.get(i-1);
+            sb.append(i).append(".").append(game.gameName());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 
-    private String playGame(String[] params, String authToken){}
+    private String createGame(String[] params, String authToken) throws Exception {
+        if (params.length >= 1) {
+            int gameID =  facade.createGame(authToken, params[0]);
+            return "Game created successfully.";
+        }
+        throw new Exception("Please provide game name.");
+    }
 
-    private String observeGame(String[] params){}
+    private String playGame(String[] params, String authToken) throws Exception{
+        if (params.length >= 2) {
+            int i = Integer.valueOf(params[0]);
+            facade.joinGame(authToken, i, params[1]);
+            PostJoin.drawBoard(params)
+        }
+        throw new Exception("enter a game number and color.");
+    }
+
+    private String observeGame(String[] params){
+        if (params.length >= 1) {
+            int gameID =  facade.joinGame(authToken, params[0], params[1]);
+        }
+        throw new Exception("Please type a username and password.");
+    }
 
 }
--- client/src/main/java/client/PreLogin.java
+++ client/src/main/java/client/PreLogin.java
@@ -1,15 +1,60 @@
 package client;
 
+import model.AuthData;
+
+import java.util.Arrays;
+
 public class PreLogin {
-    public PreLogin(ServerFacade facade){}
+    private ServerFacade facade;
+    private String authToken = null;
 
-    public String eval(String input){}
+    public PreLogin(ServerFacade facade){
+        this.facade = facade;
+    }
 
-    private String help(){}
+    public String eval(String input){
+        try {
+            String[] tokens = input.toLowerCase().split(" ");
+            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "help" -> help();
+                case "login" -> login(params);
+                case "register" -> register(params);
+                case "quit" -> "quit";
+                default -> "Unknown command. Type 'help' to see available commands.";
+            };
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
 
-    private String login(String[] params){}
+    public String help(){
+        return """
+                - help
+                - login
+                - register
+                - quit
+                """;
+    }
 
-    private String register(String[] params){}
+    private String login(String[] params) throws Exception {
+        if (params.length >= 2) {
+            AuthData result = facade.login(params[0], params[1]);
+            this.authToken = result.authToken();
+            return String.format("You logged in as %s.", params[0]);
+        }
+        throw new Exception("Please type a username and password.");
+    }
 
+    private String register(String[] params) throws Exception {
+        if (params.length >= 3) {
+            AuthData result = facade.register(params[0], params[1], params[2]);
+            this.authToken = result.authToken();
+            return String.format("You registered as %s.", params[0]);
+        }
+            throw new Exception("Please provide a username, password and email");
+    }
 
 }
+
