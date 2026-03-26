6646e45bd14b1e995e480e6befbf41f00e36ca7c
natezuro-cmyk <natezuro@gmail.com>
2026-03-26 16:30:53 -0600

    repl in progress

--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -1,4 +1,23 @@
 package client;
 
+import java.util.Scanner;
+
 public class Repl {
+    private ServerFacade facade;
+    private PreLogin preLogin;
+    private PostLogin postLogin;
+    private Scanner scanner;
+    private boolean loggedIn = false;
+    String authToken = null;
+
+    public void Repl(int port){
+        facade = new ServerFacade(port);
+        preLogin = new PreLogin(facade);
+        postLogin = new PostLogin(facade);
+    }
+
+    public void run(){
+
+    }
+
 }
