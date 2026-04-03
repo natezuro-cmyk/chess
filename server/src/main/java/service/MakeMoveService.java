fc1a19b61fba0ab76db2808b1a4beb851a9b4886
natezuro-cmyk <natezuro@gmail.com>
2026-04-03 13:44:12 -0600

    structural changes

--- server/src/main/java/service/MakeMoveService.java
+++ server/src/main/java/service/MakeMoveService.java
@@ -1,4 +1,20 @@
 package websocket.service;
 
+import chess.ChessMove;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.GameData;
+
 public class MakeMoveService {
+    private DataAccess data;
+
+    public MakeMoveService(DataAccess data){
+        this.data = data;
+    }
+
+    public void makeMove(ChessMove move, GameData gamedata) throws DataAccessException {
+        GameData game = data.getGame(gamedata.gameID());
+        makeMove(move, game);
+        data.updateGame(game);
+    }
 }
