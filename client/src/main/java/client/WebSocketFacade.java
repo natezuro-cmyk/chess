37a6bb43211d4080651707af92ecd95a9cead7bb
natezuro-cmyk <natezuro@gmail.com>
2026-04-09 02:00:13 -0600

    tons of progress, mostly on websockethandler

--- client/src/main/java/client/WebSocketFacade.java
+++ client/src/main/java/client/WebSocketFacade.java
@@ -3,10 +3,10 @@ package client;
 import com.google.gson.Gson;
 import com.sun.nio.sctp.NotificationHandler;
 import exception.ResponseException;
-import webSocketMessages.Action;
-import webSocketMessages.Notification;
 
 import jakarta.websocket.*;
+import websocket.commands.UserGameCommand;
+import websocket.messages.NotificationMessage;
 import websocket.messages.ServerMessage;
 
 import java.io.IOException;
@@ -32,34 +32,43 @@ public class WebSocketFacade extends Endpoint {
             this.session.addMessageHandler(new MessageHandler.Whole<String>() {
                 @Override
                 public void onMessage(String messageString) {
-                    try{
+                    try {
                         ServerMessage message = new Gson().fromJson(messageString, ServerMessage.class);
-                        listener.notify(message);
-                    } catch (Exception ex){
-                        listener.notify(new ErrorMessage(ex.getMessage()));
+                        switch (message.getServerMessageType()) {
+                            case UserGameCommand.CommandType.LOAD_DATA -> loadGame(message);
+                            case UserGameCommand.CommandType.ERROR -> error(message);
+                            case UserGameCommand.CommandType.NOTIFICATION -> notify(message);
+                        }
                     }
-                }
             });
         } catch (DeploymentException | IOException | URISyntaxException ex) {
             throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+            }
         }
-    }
 
     //Endpoint requires this method, but you don't have to do anything
     @Override
     public void onOpen(Session session, EndpointConfig endpointConfig) {
     }
 
-    public void makeMove(String visitorName) throws ResponseException {
+    public void loadGame(LoadGameMessage message) throws ResponseException {
         try {
-            var action = new Action(Action.Type.ENTER, visitorName);
+            var action = new (Action.Type.ENTER, visitorName);
             this.session.getBasicRemote().sendText(new Gson().toJson(action));
         } catch (IOException ex) {
             throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
         }
     }
 
-    public void leave(String visitorName) throws ResponseException {
+    public void error(ErrorMessage message) throws ResponseException {
+        try {
+            var action = new Action(Action.Type.EXIT, visitorName);
+            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+        } catch (IOException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+
+    public void notify(NotificationMessage message) throws ResponseException {
         try {
             var action = new Action(Action.Type.EXIT, visitorName);
             this.session.getBasicRemote().sendText(new Gson().toJson(action));
